e4fb2355fdedbcf96d34ec4f941990683e63ce09
Andreas Baak <andreas.baak@gmail.com>
2015-12-20 23:43:28 +0100

    Fix path names in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-TARGET_IP_ADDR=192.168.178.50
-USERNAME=root
+TARGET_IP_ADDR=192.168.1.50
+USERNAME=libipho
 
-ssh ${USERNAME}@${TARGET_IP_ADDR} "mkdir -p /home/root/photobooth"
-scp -r photobooth-scripts/* ${USERNAME}@${TARGET_IP_ADDR}:photobooth/
-scp -r html/* ${USERNAME}@${TARGET_IP_ADDR}:/www/pages/
+scp -r libipho-scripts/* ${USERNAME}@${TARGET_IP_ADDR}:libipho/
+scp -r html/* ${USERNAME}@${TARGET_IP_ADDR}:www/
 
